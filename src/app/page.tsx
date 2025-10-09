a34bb356e1c2b2ac981cf529a20f2e5f156e0db4
cordy <mglgain@gmail.com>
2025-10-09 23:33:32 +0800

    feat: Adding Header on landpage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
-
+import { 
+  Header
+} from "@/components";
 
 export default function Home() {
   return (
     <>
+      <Header />
     </>
   );
 }
--- src/components/landpage/header/index.tsx
+++ src/components/landpage/header/index.tsx
@@ -2,8 +2,22 @@ import style from "./css/styles.module.css";
 
 export default function Header() {
 	return(
-		<header>
-			
+		<header className={style.header_header}>
+			<div className={`flex ${style.container}`}>
+				<div className={style.title}>
+					<h2>Opera</h2>
+				</div>
+				<div className={`flex ${style.nav_and_buttons}`}>
+					<nav>
+						<ul className="flex">
+							<li><a href="#">Home</a></li>
+							<li><a href="#">Contacts</a></li>
+						</ul>
+					</nav>
+					<button>Sign Up</button>
+					<button>Sign In</button>
+				</div>
+			</div>
 		</header>
 	);
 }
